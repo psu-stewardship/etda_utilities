42462942c719095b1945db9b3d722208e5a3b8a0
jxb13@psu.edu <jxb13@psu.edu>
2018-01-08 11:47:43 -0500

    Upgraded ruby to 2.4.3

---
+++ etda_utilities-0.2.0/lib/etda_utilities.rb
@@ -0,0 +1,8 @@
+require 'etda_utilities/version'
+
+module EtdaUtilities
+  require 'yaml'
+  require 'etda_utilities/partner'
+  require 'etda_utilities/access_level'
+  require 'etda_utilities/etda_file_paths'
+end
