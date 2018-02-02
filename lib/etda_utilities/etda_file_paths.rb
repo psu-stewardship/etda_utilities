fd8e93390b6f7fce9d864f12b50c89f4eab5e5df
jxb13@psu.edu <jxb13@psu.edu>
2018-02-02 11:31:04 -0500

    Adding slashes to all file paths segments.

--- lib/etda_utilities/etda_file_paths.rb
+++ lib/etda_utilities/etda_file_paths.rb
@@ -4,7 +4,7 @@ module EtdaUtilities
   class EtdaFilePaths
     RESTRICTED_DIR = 'restricted'
     RESTRICTED_INSTITUTION_DIR = 'restricted_institution'
-    OPEN_DIR = 'open'
+    OPEN_DIR = 'open_access'
 
     def workflow_base_path
       path = (defined? EXPLORE_BASE_PATH) ? EXPLORE_BASE_PATH : 'tmp'
@@ -17,29 +17,39 @@ module EtdaUtilities
     end
 
     def workflow_upload_final_files_path
-      workflow_base_path + 'final-submission-files'
+      workflow_base_path + 'final_submission_files/'
     end
 
     def workflow_upload_format_review_path
-      workflow_base_path + 'format-review-files'
+      workflow_base_path + 'format_review_files/'
     end
 
     def workflow_restricted
-      workflow_base_path + RESTRICTED_DIR
+      workflow_base_path + RESTRICTED_DIR + '/'
     end
 
     def explore_open
-      explore_base_path + OPEN_DIR
+      explore_base_path + OPEN_DIR + '/'
     end
 
     def explore_psu_only
-      explore_base_path + RESTRICTED_INSTITUTION_DIR
+      explore_base_path + RESTRICTED_INSTITUTION_DIR + '/'
     end
 
     def detailed_file_path(file_id)
       str1 = format("%02d", ((file_id || 0) % 100))
       str2 = file_id.to_s
-      str1 + '/' + str2
+      str1 + '/' + str2 + '/'
+    end
+
+    def published_file_path(file_id)
+      file = FinalSubmissionFile.find(file_id)
+      submission = Submission.find(file.submission_id)
+      return nil if submission.access_level.restricted?
+      return nil unless submission.status_behavior.released_for_publication?
+      return explore_open + detailed_file_path(file.id) + file.asset if submission.access_level.open_access?
+      return explore_psu_only + detailed_file_path(file.id) + file.asset if submission.access.restricted_to_institution?
+      nil
     end
   end
 end
