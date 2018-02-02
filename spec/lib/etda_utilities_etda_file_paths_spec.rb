f768fc9acb080babf4056de37302198e1bf38f6e
jxb13@psu.edu <jxb13@psu.edu>
2018-02-02 11:31:04 -0500

    Adding slashes to all file paths segments.

--- spec/lib/etda_utilities_etda_file_paths_spec.rb
+++ spec/lib/etda_utilities_etda_file_paths_spec.rb
@@ -12,37 +12,37 @@ RSpec.describe EtdaUtilities::EtdaFilePaths, type: :model do
     end
     context 'workflow uploads' do
       it 'returns final-files path' do
-        expect(subject.workflow_upload_final_files_path).to eql('tmp/final-submission-files')
+        expect(subject.workflow_upload_final_files_path).to eql('tmp/final-submission-files/')
       end
       it 'returns format-review path' do
-        expect(subject.workflow_upload_format_review_path).to eql('tmp/format-review-files')
+        expect(subject.workflow_upload_format_review_path).to eql('tmp/format-review-files/')
       end
     end
     context '#workflow_restricted' do
       it 'returns path of published restricted files' do
-        expect(subject.workflow_restricted).to eql('tmp/restricted')
+        expect(subject.workflow_restricted).to eql('tmp/restricted/')
       end
     end
     context 'explore published paths' do
       it 'returns path of restricted to institution files' do
-        expect(subject.explore_psu_only).to eql('tmp/restricted_institution')
+        expect(subject.explore_psu_only).to eql('tmp/restricted_institution/')
       end
       it 'returns path of open_access files' do
-        expect(subject.explore_open).to eql('tmp/open')
+        expect(subject.explore_open).to eql('tmp/open/')
       end
     end
     context '#detailed_file_path' do
       it 'uses database record id to build the file path' do
         id = 2
-        expect(subject.detailed_file_path(id)).to eq('02/2')
+        expect(subject.detailed_file_path(id)).to eq('02/2/')
         id = 11
-        expect(subject.detailed_file_path(id)).to eq('11/11')
+        expect(subject.detailed_file_path(id)).to eq('11/11/')
       end
       it 'works for bigger numbers' do
         id = 345
-        expect(subject.detailed_file_path(id)).to eq('45/345')
+        expect(subject.detailed_file_path(id)).to eq('45/345/')
         id = 19857
-        expect(subject.detailed_file_path(id)).to eq('57/19857')
+        expect(subject.detailed_file_path(id)).to eq('57/19857/')
       end
     end
   end
