61b26a2018461a2936ecc39978b7d8fc50635695
Alex Kiessling <ajkiessl@gmail.com>
2020-01-22 12:27:54 -0500

    ETDA-1643 Using erb properly now.

--- lib/etda_utilities.rb
+++ lib/etda_utilities.rb
@@ -4,6 +4,7 @@ require 'etda_utilities/version'
 
 module EtdaUtilities
   require 'yaml'
+  require 'erb'
   require 'etda_utilities/partner'
   require 'etda_utilities/access_level'
   require 'etda_utilities/etda_file_paths'
